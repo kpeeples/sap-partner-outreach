2b0669a46eed2183cef181ace10641ba7ce21643
Kenneth Peeples <kpeeples@redhat.com>
2013-08-24 14:21:33 -0400

    modify and test first screen

--- android-and-fuse/android-dv-fuse/gen/com/example/demo_dv_fuse/R.java
+++ android-and-fuse/android-dv-fuse/gen/com/example/demo_dv_fuse/R.java
@@ -8,6 +8,9 @@
 package com.example.demo_dv_fuse;
 
 public final class R {
+    public static final class array {
+        public static final int country_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,25 +25,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int redhat=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080002;
-        public static final int expandableListView1=0x7f080004;
-        public static final int progressBar1=0x7f080003;
-        public static final int spinner1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int GridView1=0x7f090000;
+        public static final int action_settings=0x7f090006;
+        public static final int button1=0x7f090003;
+        public static final int listView1=0x7f090005;
+        public static final int progressBar2=0x7f090004;
+        public static final int spinner1=0x7f090001;
+        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int airports=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int retrieve_airport_flights=0x7f050004;
     }
     public static final class style {
         /** 
@@ -64,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
